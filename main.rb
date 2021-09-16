998b3ac49176e35d11e7b5238e1fdeb4fca22807
shutheonetokyo <ss1030ss@icloud.com>
2021-09-16 11:02:38 +0900

    Monsterクラスの作成

--- main.rb
+++ main.rb
@@ -24,3 +24,19 @@ TEXT
 brave.hp -= 30
 
 puts "#{brave.name}はダメージを受けた!　残りHPは#{brave.hp}だ"
+
+
+class Monster
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+
+end
+
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
