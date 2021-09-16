80698178f44abd1f4be2fd15c406bddd04280e76
shutheonetokyo <ss1030ss@icloud.com>
2021-09-16 15:02:27 +0900

    モンスターの攻撃処理の実装

--- main.rb
+++ main.rb
@@ -38,14 +38,48 @@ end
 
 
 class Monster
-  attr_reader :name, :offense, :defense
-  attr_accessor :hp
+  attr_reader :offense, :defense
+  attr_accessor :hp, :name
+
+  POWER_UP_RATE = 1.5
+
+  CALC_HALF_HP = 0.5
 
   def initialize(**params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * 0.5
+  end
+
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+    puts "#{@name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage 
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+  def transform
+    transform_name = "ドラゴン"
+
+    puts <<~EOS
+    #{@name}は怒っている
+    #{@name}は#{transform_name}に変身した
+    EOS
+
+    @offense *= POWER_UP_RATE
+    @name = transform_name
   end
 end
 
@@ -53,3 +87,5 @@ brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 brave.attack(monster)
+
+monster.attack(brave)
