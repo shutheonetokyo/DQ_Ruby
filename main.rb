fe1381ef131dddb136636836454ee2814c0ab064
shutheonetokyo <ss1030ss@icloud.com>
2021-09-16 14:18:05 +0900

    必殺攻撃処理の実装

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 class Brave
   attr_reader :name, :offense, :defense
-  # セッターゲッターを一括定義
   attr_accessor :hp
+
+  SPECIAL_ATTACK_CONSTANT = 1.5
 
   def initialize(**params)
     @name = params[:name]
@@ -13,15 +14,27 @@ class Brave
   def attack(monster)
     puts "#{@name}の攻撃"
 
-    damage = @offense - monster.defense
+    attack_num = rand(4)
+
+    if attack_num == 0
+      puts "必殺攻撃"
+      damage = calculate_special_attack - monster.defense
+    else
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
     monster.hp -= damage
 
     puts "#{monster.name}は#{damage}のダメージを受けた"
     puts "#{monster.name}の残りHPは#{monster.hp}だ"
   end
 
-end
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
 
+  end
+end
 
 
 class Monster
@@ -34,12 +47,9 @@ class Monster
     @offense = params[:offense]
     @defense = params[:defense]
   end
-
-
 end
 
 brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
 monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 
 brave.attack(monster)
-
