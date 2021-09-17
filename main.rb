07757d7b62d0cd4149eef6e8b5921aaf201d0546
shutheonetokyo <ss1030ss@icloud.com>
2021-09-17 16:19:46 +0900

    戦闘後の結果処理の実装

--- main.rb
+++ main.rb
@@ -132,7 +132,19 @@ monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
 loop do
   brave.attack(monster)
   break if monster.hp <= 0
-  
+
   monster.attack(brave)
-  break if brave.hp  <= 0
+  break if brave.hp <= 0
+end
+
+battle_result = brave.hp > 0
+
+if battle_result
+  exp = (monster.offense + monster.defense) * 2
+  gold = (monster.offense + monster.defense) * 3
+  puts "#{brave.name}はたたかいに勝った"
+  puts "#{exp}の経験値と#{gold}ゴールドを獲得した"
+else
+  puts "#{brave.name}はたたかいに負けた"
+  puts "目の前が真っ暗になった"
 end
