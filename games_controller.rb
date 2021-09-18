b22fb92165460fcd52ed6b55d3e353887a7450ac
shutheonetokyo <ss1030ss@icloud.com>
2021-09-18 23:14:54 +0900

    games_controller.rbのリファクタリング

--- games_controller.rb
+++ games_controller.rb
@@ -4,40 +4,51 @@ class GamesController
   GOLD_CONSTANT = 3
 
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    
+    build_characters(**params)
 
     loop do
-      brave.attack(monster)
-      break if battle_end?(monster)
-      monster.attack(brave)
-      break if battle_end?(brave)
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
     end
     
-    if battle_result(brave)
-      result = calculate_of_exp_and_gold(monster)
-      puts "#{brave.name}はたたかいに勝った"
-      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
-    else
-      puts "#{brave.name}はたたかいに負けた"
-      puts "目の前が真っ暗になった"
-    end
+    battle_judgement
   end
   
   private
 
-  def battle_end?(character)
-    character.hp <= 0
+  def build_characters(**params)
+    @brave = params[:brave]
+    @monster = params[:monster]
   end
 
-  def battle_result(brave)
-    brave.hp > 0
+  def battle_end?
+    @brave.hp <= 0 || @monster.hp <= 0
   end
 
-  def calculate_of_exp_and_gold(monster)
-    exp = (monster.offense + monster.defense) * EXP_CONSTANT
-    gold = (monster.offense + monster.defense) * GOLD_CONSTANT
+  def brave_win?
+    @brave.hp > 0
+  end
+
+  def battle_judgement
+    if brave_win?
+      result = calculate_of_exp_and_gold
+      puts "#{@brave.name}はたたかいに勝った"
+      puts "#{result[:exp]}の経験値と#{result[:gold]}ゴールドを獲得した"
+    else
+      puts "#{@brave.name}はたたかいに負けた"
+      puts "目の前が真っ暗になった"
+    end
+  end
+
+  def calculate_of_exp_and_gold
+    exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+    gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
     result = {exp: exp, gold: gold}
+    
+    result
   end
 
 end
